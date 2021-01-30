dbd255dbcbdf002b636ed2a2853da4e0b82df840
ginery <songalingginery@gmail.com>
2021-01-30 11:27:23 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,13 +1,13 @@
 <?php 
 
 
-//localhost
-$con = mysql_connect('localhost','root','');
-mysql_select_db('cml', $con);
+// //localhost
+// $con = mysql_connect('localhost','root','');
+// mysql_select_db('cml', $con);
 
 //online
-// $con = mysql_connect('localhost','id14848713_dev_cml','<Yc5<Gj$IFphVQ{v');
-// mysql_select_db('id14848713_cml', $con);
+$con = mysql_connect('localhost','id14848713_dev_cml','<Yc5<Gj$IFphVQ{v');
+mysql_select_db('id14848713_cml', $con);
 
 ////////////////////////////////
 include "gFunction.php";
