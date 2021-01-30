191baf6e0fabbf4c763611bb462ed84ae6518625
ginery <songalingginery@gmail.com>
2021-01-30 11:31:43 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -6,8 +6,8 @@
 // mysql_select_db('cml', $con);
 
 //online
-$con = mysql_connect('localhost','id14848713_dev_cml','<Yc5<Gj$IFphVQ{v');
-mysql_select_db('id14848713_cml', $con);
+$con = mysql_connect('localhost','u584261719_cml','pwo/OSM60~Df');
+mysql_select_db('u584261719_cml', $con);
 
 ////////////////////////////////
 include "gFunction.php";
