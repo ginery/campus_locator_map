57108eb01dd3a2b799ea6c9871f3d5b73147ed27
Ginery <songalingginery@gmail.com>
2021-02-03 11:14:21 +0800

    a

--- ajax/add_building.php
+++ ajax/add_building.php
@@ -10,7 +10,7 @@ $no_s1 = safe($_POST['no_steps_1']);
 $no_s2 = safe($_POST['no_steps_2']);
 
 
-$result = mysql_query("INSERT INTO `cml`.`tbl_building` (`building_name`, `desc`, `floor`, `distance_g1`, `distance_g2`, `no_steps_1`, `no_steps_2`) VALUES ('$building_name', '$desc', '$floor_no', '$d_g1', '$d_g2', '$no_s1', '$no_s2')");
+$result = mysql_query("INSERT INTO `tbl_building` (`building_name`, `desc`, `floor`, `distance_g1`, `distance_g2`, `no_steps_1`, `no_steps_2`) VALUES ('$building_name', '$desc', '$floor_no', '$d_g1', '$d_g2', '$no_s1', '$no_s2')");
 if ($result) {
 	echo 1;
 }else{
