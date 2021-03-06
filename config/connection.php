1e1f2b76333175d98aedacfc88c58d2a1df34197
ginery <songalingginery@gmail.com>
2021-03-06 22:17:18 +0800

    a

--- config/connection.php
+++ config/connection.php
@@ -6,8 +6,8 @@
 // mysql_select_db('cml', $con);
 
 //online
-$con = mysql_connect('localhost','u584261719_cml','pwo/OSM60~Df');
-mysql_select_db('u584261719_cml', $con);
+$con = mysql_connect('localhost','root','');
+mysql_select_db('cml', $con);
 
 ////////////////////////////////
 include "gFunction.php";
